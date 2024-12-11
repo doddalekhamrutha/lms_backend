068d50ada9f3dc71c45bfbb8d5b47b98bd8b6966
Vyshnavi_Mylavarapu <149768150+vyshnaviMylavarapu@users.noreply.github.com>
2024-12-11 14:02:26 +0530

    Update WebConfig.java

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,4 +1,4 @@
-package com.klef.jfsd.hospital;
+package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
